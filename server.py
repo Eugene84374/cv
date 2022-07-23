289a0ea5a76cfd3f9559373a318a735482d410d0
iameugenes <58175079+iameugenes@users.noreply.github.com>
2022-07-23 05:57:49 -0700

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/picorp')
+def applink():
+    return render_template("picorp.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
