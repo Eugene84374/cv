bd9a1f32f0df0db15ade0899b3c34fd7f109be0d
iameugenes <58175079+iameugenes@users.noreply.github.com>
2022-07-23 06:21:54 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("picorp.html")
+    return render_template("index.html")
 
 @app.route('/picorp')
 def applink():
-    return render_template("index.html")
+    return render_template("picorp.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
